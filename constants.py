9b62e166a2f91f37dfd360a93349bfa0f1a51a10
KadinTucker <kadintucker@gmail.com>
2019-12-12 10:24:20 +0100

    Add .gitignore and file containing constants definition

---
+++ constants.py
@@ -0,0 +1,11 @@
+"""
+File which defines the necessary constants for use in computations
+"""
+
+DIAMETER = 0.24 #Meters
+MASS = 0.6 #Kilograms
+AIR_RES_C = 0.45
+AIR_DENSITY = 1.23 #kilogram / meter cubed
+BASKET_HEIGHT = 3.05 #meters
+PLAYER_DISTANT = 2.0 #meters
+GRAVITY = 9.81 #Meters per second squared
